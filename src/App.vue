ac6aabfb98ccc5d673ba5223b3a0ea30d82b8899
Benjamin Fritzeen <bfritzeen@benjamins-mini.campus.tayloru.edu>
2020-02-19 18:39:33 -0500

    Added DayButtons & changed coloring

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ export default {
 
 <style>
 #app {
+  overflow-y: hidden;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/DayBoxes.vue
+++ src/components/DayBoxes.vue
@@ -1,13 +1,13 @@
-<template class = "main">
+<template>
     <div class ="main">
-        <b-container fluid class="h-100 border border-danger">
-            <b-button name="Monday" class="btn btn-lg"/>
-            <b-button name="Tuesday"/>
-            <b-button name="Wednesday"/>
-            <b-button name="Thursday"/>
-            <b-button name="Friday"/>
-            <b-button name="Saturday"/>
-            <b-button name="Sunday"/>
+        <b-container fluid class="h-100">
+            <b-button variant="primary" name="Monday" class="btn btn-lg">Monday</b-button>
+            <b-button variant="primary" name="Tuesday" class="btn btn-lg">Tuesday</b-button>
+            <b-button variant="primary" name="Wednesday" class="btn btn-lg">Wednesday</b-button>
+            <b-button variant="primary" name="Thursday" class="btn btn-lg">Thursday</b-button>
+            <b-button variant="primary" name="Friday" class="btn btn-lg">Friday</b-button>
+            <b-button variant="primary" name="Saturday" class="btn btn-lg">Saturday</b-button>
+            <b-button variant="primary" name="Sunday" class="btn btn-lg">Sunday</b-button>
         </b-container>
     </div>
 </template>
@@ -15,6 +15,6 @@
 <style scoped>
 .main {
     height: 100vh;
-    background-color: green;
+    background-color: lightgray;
 }
 </style>
