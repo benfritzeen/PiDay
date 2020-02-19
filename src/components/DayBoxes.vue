e7fe75291590f75c5e84fe589c8ecc70efc5014a
Benjamin Fritzeen <bfritzeen@benjamins-mini.campus.tayloru.edu>
2020-02-19 17:45:26 -0500

    Testing sizing

--- src/components/DayBoxes.vue
+++ src/components/DayBoxes.vue
@@ -1,11 +1,20 @@
-<template>
-    <div>
-        <b-button name="Monday"/>
-        <b-button name="Tuesday"/>
-        <b-button name="Wednesday"/>
-        <b-button name="Thursday"/>
-        <b-button name="Friday"/>
-        <b-button name="Saturday"/>
-        <b-button name="Sunday"/>
+<template class = "main">
+    <div class ="main">
+        <b-container fluid class="h-100 border border-danger">
+            <b-button name="Monday" class="btn btn-lg"/>
+            <b-button name="Tuesday"/>
+            <b-button name="Wednesday"/>
+            <b-button name="Thursday"/>
+            <b-button name="Friday"/>
+            <b-button name="Saturday"/>
+            <b-button name="Sunday"/>
+        </b-container>
     </div>
 </template>
+
+<style scoped>
+.main {
+    height: 100vh;
+    background-color: green;
+}
+</style>
