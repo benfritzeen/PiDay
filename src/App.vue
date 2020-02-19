8debca3794fe8a2b36e7644e0761e175498e1102
Benjamin Fritzeen <bfritzeen@benjamins-mini.campus.tayloru.edu>
2020-02-19 16:01:11 -0500

    Added Boxes (for testing)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DayBoxes />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DayBoxes from './components/DayBoxes.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DayBoxes,
   }
 }
 </script>
---
+++ src/components/DayBoxes.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <b-button name="Monday"/>
+        <b-button name="Tuesday"/>
+        <b-button name="Wednesday"/>
+        <b-button name="Thursday"/>
+        <b-button name="Friday"/>
+        <b-button name="Saturday"/>
+        <b-button name="Sunday"/>
+    </div>
+</template>
